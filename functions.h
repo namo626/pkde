12d54191e5d8f54b389eaee1463baf79f33e1ff1
Chayanon Wichitrnithed <namo@login1.frontera.tacc.utexas.edu>
2023-04-23 15:15:23 -0500

    Fix merge

--- functions.h
+++ functions.h
@@ -3,11 +3,7 @@
 
 #include <math.h>
 
-<<<<<<< HEAD
-#define Nx (int)pow(2,18)
-=======
 #define Nx (int)pow(2,20)
->>>>>>> refs/remotes/origin/master
 #define Ny Nx
 #define h (3.49*1 / pow((float)Ny, 0.333))
 
