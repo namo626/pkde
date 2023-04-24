576df1ac7b10634f47fd0cd4dd6dc926257c28e3
rs821505 <107643011+rs821505@users.noreply.github.com>
2023-04-23 19:31:49 -0500

    updated file path to be written too

--- functions.cpp
+++ functions.cpp
@@ -5,29 +5,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void writeOutput(const char *fname, float *xs, float *ys, float *fs)
+{
 
-void writeOutput(const char* fname, float* xs, float* ys, float* fs) {
-    FILE* fpt;
-    fpt = fopen(fname, "w");
-    if (fpt == NULL) {
+    char path[100];                                     // assuming a maximum path length of 100 characters
+    snprintf(path, sizeof(path), "data/raw/%s", fname); // appending "data/raw/" to the original
+
+    FILE *fpt;
+    fpt = fopen(path, "w"); // opening the file with the modified path
+
+    if (fpt == NULL)
+    {
         printf("Failed to open file: %s\n", fname);
         return;
     }
-    for (int i = 0; i < Nx; i++) {
+    for (int i = 0; i < Nx; i++)
+    {
         fprintf(fpt, "%.7f, %.7f, %.7f\n", xs[i], ys[i], fs[i]);
     }
     fclose(fpt);
 }
 
-
-void printElapsedTime(clock_t tic, clock_t toc) {
+void printElapsedTime(clock_t tic, clock_t toc)
+{
     double elapsedSeconds = (double)(toc - tic) / CLOCKS_PER_SEC;
     printf("Elapsed: %f seconds\n", elapsedSeconds);
 }
 
-
-void checkFloatArray(float* fs) {
-    for (int i = 0; i < Nx; i++) {
+void checkFloatArray(float *fs)
+{
+    for (int i = 0; i < Nx; i++)
+    {
         assert(fabs(fs[i]) <= 1e-8);
     }
 }
